f9aa4c6b813e314c9cd0d6f3506a0b50a72fbf5a
Nagaraju Kusa <nagaraju.kusa@thoughtworks.com>
2023-12-27 22:12:49 +0530

    Added basic shell script for practice

---
+++ shell-scripting/main.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# shebang
+
+# i want to create a file. and I want to add some content into that file
+# -> comment
+
+# create a new file
+touch first-file.txt
+touch second-file.txt
+
+# add some content into that file
+echo "Good evening" > first-file.txt
+
+# view content present inside a file
+cat first-file.txt
